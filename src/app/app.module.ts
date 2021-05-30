99d9cbd05142b73b78526bd124463c2c1a9ab454
markodojkic <marko.dojkic@gmail.com>
2021-05-30 14:15:44 +0200

    Created services files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,9 @@
+import { OrderService } from './services/order/order.service';
+import { ProductReviewService } from './services/product/product-review.service';
+import { ProductService } from './services/product/product.service';
+import { CategoryService } from './services/product/category.service';
+import { OrderProductService } from './services/order/order-product.service';
+import { AccountService } from './services/account/account.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,7 +14,7 @@ import { RoutingModule } from './routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CryptoService } from './auth/crypto/crypto.service';
+import { CryptoService } from './services/crypto/crypto.service';
 import { ProductsComponent } from './main/products/products.component';
 import { OrdersComponent } from './main/orders/orders.component';
 import { AccountsComponent } from './main/accounts/accounts.component';
@@ -31,7 +37,8 @@ import { AccountsComponent } from './main/accounts/accounts.component';
   ],
 
   
-  providers: [Title, CryptoService],
+  providers: [Title, CryptoService, AccountService, CategoryService,
+    ProductService, ProductReviewService, OrderService, OrderProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
